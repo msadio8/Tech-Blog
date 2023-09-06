087ca330f427a7b803e7444f6a429aaeb15f6647
Meriam <132971614+msadio8@users.noreply.github.com>
2023-09-06 21:02:59 +1000

    add comments js

---
+++ models/comments.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    commentText: {
+      type: DataTypes.STRING, // data type of the column in our table
+      allowNull: false, // if not null then it will be required to enter a value for this field when creating an entry
+      validate: {
+        len: [1],
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      }, // foreign key reference to user model and its 'id' field
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+module.exports = Comment;
